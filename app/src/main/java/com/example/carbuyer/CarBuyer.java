e14bd1cf312db1da810ee87dbc2d51b071ae3f81
AtharvaM2003 <atharva2mundhe@gmail.com>
2024-03-17 15:14:31 +0530

    done

---
+++ app/src/main/java/com/example/carbuyer/CarBuyer.java
@@ -0,0 +1,23 @@
+package com.example.carbuyer;
+public class CarBuyer {
+    public String name;
+    public String mbno;
+    public String mail;
+    public String cpass;
+
+    public String age;
+    public String gender;
+
+    public CarBuyer(String name, String mbno, String mail, String cpass, String age , String gender){
+        this.name=name;
+        this.mbno=mbno;
+        this.mail=mail;
+        this.cpass=cpass;
+
+        this.age=age;
+        this.gender=gender;
+
+    }
+
+}
+
